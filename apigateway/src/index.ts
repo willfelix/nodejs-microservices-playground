d48effdd96816fedf382528d833198bf0ba72891
Will Felix <will.felix@thoughtworks.com>
2022-08-06 05:39:23 -0300

    Merge

--- apigateway/src/index.ts
+++ apigateway/src/index.ts
@@ -1,16 +1,14 @@
 import "dotenv/config";
-
 import express from "express";
-import { AddressInfo } from "net";
 
 import routes from "./routes";
 
+const port = 3001;
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 
-const listener = app.listen(3001, () => {
-  const address = listener.address() as AddressInfo;
-  console.log(`Server running on ${address.port}`);
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
 });
